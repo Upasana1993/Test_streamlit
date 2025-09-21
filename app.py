0b8e0ad316831e6c5a03812d14789425adc5d7ef
unknown <abhishekghsh8@gmail.com>
2025-09-21 20:03:16 +0530

    update app.py with new changes

--- app.py
+++ app.py
@@ -1,3 +1,8 @@
 import streamlit as st
+
 st.title("Hello, World!")
 st.write("This is my first Streamlit app 🚀")
+
+name = st.text_input("What's your name?")
+if name:
+    st.success(f"Welcome, {name}!")
